f830b44785573f166dbc0cff430449ec6ebe3160
George Reznik <48242089+Reznx@users.noreply.github.com>
2020-05-22 08:47:29 +0500

    framework start

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,20 @@
+export class Excel {
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector);
+    this.components = options.components || [];
+  }
+
+  getRoot() {
+    const $root = document.createElement("div");
+    this.components.forEach((Component) => {
+      const component = new Component();
+      // afterbegin, afterend, beforeend, beforebegin
+      $root.insertAdjacentHTML("beforeend", component.toHTML());
+    });
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
+  }
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,9 @@
+import { DomListener } from "@core/DomListener";
+
+// Корневой компонент
+export class ExcelComponent extends DomListener {
+  // Возвращает шаблон компонента
+  toHTML() {
+    return "";
+  }
+}

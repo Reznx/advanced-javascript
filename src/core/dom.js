38f4ee7e5cedefdbd937bcab4219e66808da5ef2
George Reznik <48242089+Reznx@users.noreply.github.com>
2020-05-26 10:41:21 +0500

    dom class

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,43 @@
 class Dom {
-  constructor() {}
+  constructor(selector) {
+    // #app
+    this.$el =
+      typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector;
+  }
+
+  html(html) {
+    if (typeof html === "string") {
+      this.$el.innerHTML = html;
+      // Для того, чтобы вернуть инстанс, на котором может выполнятся методы строки
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html("");
+    // Для того, чтобы вернуть инстанс, на котором может выполнятся методы строки
+    return this;
+  }
+  // Елемент
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+    return this;
+  }
 }
 
-export function $() {
-  return new Dom();
+// event.target
+export function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = "") => {
@@ -11,5 +45,5 @@ $.create = (tagName, classes = "") => {
   if (classes) {
     el.classList.add(classes);
   }
-  return el;
+  return $(el);
 };
